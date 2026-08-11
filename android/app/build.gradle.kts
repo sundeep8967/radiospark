dd779349f4f34630e74434f5f01f62a6e48f8561
sundeep8967 <sundeep8967@gmail.com>
2026-08-11 23:39:26 +0530

    chore: update android namespace to com.ravana.radiostation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.radiopark_clone"
+    namespace = "com.ravana.radiostation"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
